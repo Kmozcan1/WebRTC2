bababab713201ad7e7aac34e158fe2b0e71699a8
kmozcan1 <kmozcan1@gmail.com>
2020-01-14 18:18:03 +0100

    codec option

--- app/src/main/java/com/vox/sample/webrtc/Client.kt
+++ app/src/main/java/com/vox/sample/webrtc/Client.kt
@@ -22,6 +22,17 @@ import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_stream.view.*
 import org.json.JSONException
+import java.util.*
+import java.util.Arrays.asList
+import java.util.regex.Pattern
+import java.util.Arrays.asList
+import java.util.Arrays.asList
+
+
+
+
+
+
 
 
 class Client constructor (var client: Socket, private val context: Context, private val peerConnectionManager: PeerConnectionManager, private val mode: String): SocketInterface {
@@ -31,6 +42,7 @@ class Client constructor (var client: Socket, private val context: Context, priv
     private var localPeer: PeerConnection? = null
     private val executor = Executors.newSingleThreadExecutor()
     private lateinit var handler: Handler
+    var firstTime = true
     private var saveRecordedAudioToFile: RecordedAudioToFileController? = null
 
     init {
@@ -60,7 +72,15 @@ class Client constructor (var client: Socket, private val context: Context, priv
                 object : CustomPeerConnectionObserver("localPeerCreation") {
                     override fun onIceCandidate(iceCandidate: IceCandidate) {
                         super.onIceCandidate(iceCandidate)
-                        onIceCandidateReceived(iceCandidate)
+                        if (mode == "presenter") {
+                            onIceCandidateReceived(iceCandidate)
+                        } else {
+                            if (firstTime) {
+                                firstTime = false
+
+                            }
+                        }
+
                     }
 
                     override fun onAddTrack(
@@ -71,20 +91,14 @@ class Client constructor (var client: Socket, private val context: Context, priv
                         if (mode == "presenter") {
                             mediaStreams!![0].audioTracks[0].setEnabled(false)
                         } else {
-                            runOnUiThread {
-                                val recordButton =
-                                    (context as StreamActivity).findViewById<View>(R.id.record_button) as Button
-                                val statusTextView =
-                                    (context).findViewById<View>(R.id.status_text_view) as TextView
-                                recordButton.visibility = View.VISIBLE
-                                statusTextView.text = "listening"
-                            }
+
                         }
                         Log.e("audio", mediaStreams!![0].audioTracks[0].toString())
-                        super.onAddTrack(rtpReceiver, mediaStreams)
                     }
                 })!!
-            addStreamToLocalPeer()
+            if (mode == "presenter") {
+                addStreamToLocalPeer()
+            }
         }
     }
 
@@ -181,6 +195,14 @@ class Client constructor (var client: Socket, private val context: Context, priv
                     signalingMessage.sessionDescription?.sdp
                 )
             )
+            runOnUiThread {
+                val recordButton =
+                    (context as StreamActivity).findViewById<View>(R.id.record_button) as Button
+                val statusTextView =
+                    (context).findViewById<View>(R.id.status_text_view) as TextView
+                recordButton.visibility = View.VISIBLE
+                statusTextView.text = "listening"
+            }
             answer()
         }
     }
@@ -196,6 +218,7 @@ class Client constructor (var client: Socket, private val context: Context, priv
                 sendSdp(sessionDescription)
             }
         }, MediaConstraints())
+
     }
 
     override fun onAnswerReceived(signalingMessage: SignalingMessage) {
@@ -249,16 +272,99 @@ class Client constructor (var client: Socket, private val context: Context, priv
         executor.execute {
             localPeer!!.createOffer(object : CustomSdpObserver("localCreateOffer") {
                 override fun onCreateSuccess(sessionDescription: SessionDescription) {
-                    super.onCreateSuccess(sessionDescription)
+                    var sdpDescription = preferCodec(sessionDescription.description, "opus", true)
+                    var sdpRemote =  SessionDescription(sessionDescription.type, sdpDescription)
+                    super.onCreateSuccess(sdpRemote)
+
                     localPeer!!.setLocalDescription(
                         CustomSdpObserver("localSetLocalDesc"),
-                        sessionDescription
+                        sdpRemote
                     )
                     Log.e("onCreateSuccess", "SignallingClient emit ")
-                    sendSdp(sessionDescription)
+                    sendSdp(sdpRemote)
                 }
             }, sdpConstraints)
         }
+    }
+
+    private fun preferCodec(sdpDescription: String, codec: String, isAudio: Boolean): String {
+        val lines =
+            sdpDescription.split("\r\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val mLineIndex = findMediaDescriptionLine(isAudio, lines)
+        if (mLineIndex == -1) {
+            Log.w("asd", "No mediaDescription line, so can't prefer $codec")
+            return sdpDescription
+        }
+        // A list with all the payload types with name |codec|. The payload types are integers in the
+        // range 96-127, but they are stored as strings here.
+        val codecPayloadTypes = arrayListOf<String>()
+        // a=rtpmap:<payload type> <encoding name>/<clock rate> [/<encoding parameters>]
+        val codecPattern = Pattern.compile("^a=rtpmap:(\\d+) $codec(/\\d+)+[\r]?$")
+        for (line in lines) {
+            val codecMatcher = codecPattern.matcher(line)
+            if (codecMatcher.matches()) {
+                codecPayloadTypes.add(codecMatcher.group(1))
+            }
+        }
+        if (codecPayloadTypes.isEmpty()) {
+            Log.w("asd", "No payload types with name $codec")
+            return sdpDescription
+        }
+
+        val newMLine = movePayloadTypesToFront(codecPayloadTypes, lines[mLineIndex])
+            ?: return sdpDescription
+        Log.d("asd", "Change media description from: " + lines[mLineIndex] + " to " + newMLine)
+        lines[mLineIndex] = newMLine
+        return joinString(lines.toList(), "\r\n", true /* delimiterAtEnd */);
+    }
+
+    /** Returns the line number containing "m=audio|video", or -1 if no such line exists.  */
+    private fun findMediaDescriptionLine(isAudio: Boolean, sdpLines: Array<String>): Int {
+        val mediaDescription = if (isAudio) "m=audio " else "m=video "
+        for (i in sdpLines.indices) {
+            if (sdpLines[i].startsWith(mediaDescription)) {
+                return i
+            }
+        }
+        return -1
+    }
+
+    private fun movePayloadTypesToFront(
+        preferredPayloadTypes: List<String>, mLine: String
+    ): String? {
+        // The format of the media description line should be: m=<media> <port> <proto> <fmt> ...
+        val origLineParts = mLine.split(" ")
+        if (origLineParts.size <= 3) {
+            Log.e("asd", "Wrong SDP media description format: $mLine")
+            return null
+        }
+        val header = origLineParts.subList(0, 3)
+        val unpreferredPayloadTypes = ArrayList(origLineParts.subList(3, origLineParts.size))
+        unpreferredPayloadTypes.removeAll(preferredPayloadTypes)
+        // Reconstruct the line with |preferredPayloadTypes| moved to the beginning of the payload
+        // types.
+        val newLineParts = arrayListOf<String>()
+        newLineParts.addAll(header)
+        newLineParts.addAll(preferredPayloadTypes)
+        newLineParts.addAll(unpreferredPayloadTypes)
+        return joinString(newLineParts, " ", false /* delimiterAtEnd */)
+    }
+
+    private fun joinString(
+        s: Iterable<CharSequence>, delimiter: String, delimiterAtEnd: Boolean
+    ): String {
+        val iter = s.iterator()
+        if (!iter.hasNext()) {
+            return ""
+        }
+        val buffer = StringBuilder(iter.next())
+        while (iter.hasNext()) {
+            buffer.append(delimiter).append(iter.next())
+        }
+        if (delimiterAtEnd) {
+            buffer.append(delimiter)
+        }
+        return buffer.toString()
     }
 
     override fun connectionInitialized() {
